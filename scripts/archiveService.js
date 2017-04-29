a132fc50fb160d15202d9882d3aefb21fc950e9e
Ionut <ionutzbalazs@hotmail.com>
2017-04-29 17:55:35 +0300

    services

---
+++ scripts/archiveService.js
@@ -0,0 +1,32 @@
+ArchiveService = function(apiKey){
+    var _url = "https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=" + apiKey;
+    
+    this.get = function (lat, long, callback) {
+        var requestUrl = _url + "&lat=" + lat + "&lon=" + long;
+        $.get(requestUrl,
+              function(data) {
+                  var marker = computeMarker(data);
+                  callback(marker);
+              });
+    }
+
+    function computeMarker(satelliteData) {
+        return {
+            lat: satelliteData.inputs.lat,
+            long: satelliteData.inputs.lon,
+            dni: {
+                annual: computeEnergy(satelliteData.outputs.avg_dni.annual),
+            },
+            ghi: {
+                annual: computeEnergy(satelliteData.outputs.avg_ghi.annual),
+            },
+            lat_tilt: {
+                annual: computeEnergy(satelliteData.outputs.avg_lat_tilt.annual),
+            }
+        }
+    }
+
+    function computeEnergy(irradience) {
+        return irradience * 0.17 * 0.75;
+    }
+}
--- scripts/forecastService.js
+++ scripts/forecastService.js
@@ -1,11 +1,66 @@
 ForecastService = function (apiKey) {
-    var _url = String.format("https://api.solcast.com.au/pv_power/forecasts?capacity=1000&api_key={0}&format=json", apiKey);
+    var _url = "https://api.solcast.com.au/pv_power/forecasts?capacity=200&format=json&api_key=" + apiKey;
     
-    this.get = function (lat, long) {
-        var requestUrl = _url + String.format("&longitude={0}&latitude={1}", long, lat);
-        $.get(requestUrl,
-              function(data) {
-                  // do stuff
-              });
+    Date.prototype.addHours = function(h) {    
+        this.setTime(this.getTime() + (h*60*60*1000)); 
+        return this;   
+    }
+
+    this.get = function (lat, long, callback) {
+        var requestUrl = _url + "&longitude=" + long + "&latitude=" + lat;
+        $.ajax({
+            type: 'GET',
+            url: requestUrl,
+            success: function(data) {
+                  forecast = computePoint(data, long);
+                  callback(forecast)
+              }
+        });
+    }
+
+    function computePoint(satelliteData, longitude) {
+        var result = [];
+        var currentDay = { forecasts: [] };
+
+        for (var i = 0; i < satelliteData.forecasts.length; i++) {
+            lastForecast = $(currentDay.forecasts).get(-1);
+            var date = computeLocalTime(longitude, satelliteData.forecasts[i].period_end);
+
+            if (lastForecast && (lastForecast.endPeriod.getDay() != date.getDay()))
+            {
+                result.push(currentDay);
+                currentDay = { forecasts: [] };
+            }
+            
+            currentDay.forecasts.push({
+                endPeriod: date,
+                pv_estimate: satelliteData.forecasts[i].pv_estimate
+            });
+        }
+
+        return result;
+    }
+
+    function computeLocalTime(long, dateStr) {
+        var timestamp = Date.parse(dateStr);
+        var date = new Date(dateStr); // GMT+3 (local)
+
+        if (long >= -60 && long < -45) {
+            date = date.addHours(-6);
+        }
+        if (long >= -75 && long < -60) {
+            date = date.addHours(-7);
+        }
+        if (long >= -90 && long < -75) {
+            date = date.addHours(-8);
+        }
+        if (long >= -105 && long < -90) {
+            date = date.addHours(-9);
+        }
+        if (long >= -120 && long < -105) {
+            date = date.addHours(-10);
+        }
+
+        return date;
     }
 }
